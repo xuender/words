a080c89c4c988468696b21e2a7d7a28306e3f15b
徐晨阳 <xuender@gmail.com>
2018-03-01 21:10:03 +0800

    d1

---
+++ src/dict.ts
@@ -0,0 +1,4 @@
+export interface Dict {
+    txt: string;
+    pinyin: string[][];
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
 console.log('test')
+const pinyin = require("pinyin");
+console.log(pinyin("中心"));
+console.log('2', pinyin("中心", {
+    style: pinyin.STYLE_INITIALS,
+    heteronym: true
+}));
